c705acf191092bc799dd2b28c64f659c9d6d7462
LeeKyungHoon <38177235+LeeKyungHoon@users.noreply.github.com>
2018-07-20 23:01:37 +0900

    20180720

--- blockchain/Transaction.h
+++ blockchain/Transaction.h
@@ -1,17 +1,29 @@
 
 
+//Tx 넣어야 할 거 : nonce, Value, toAddress, transaction, signature
+//essential functions : signature function
+
+
+enum class TransactionData {
+
+};
+
+template<typename T>
 class Transaction {
 public :
+
+
 	Transaction();
 	void setNull();
 
 };
-
-Transaction::Transaction() {
+template<typename T>
+Transaction<T>::Transaction() {
 	setNull();
 }
 
-void Transaction::setNull() {
+template<typename T>
+void Transaction<T>::setNull() {
 
 
 };
