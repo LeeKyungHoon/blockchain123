006577a74718e7268701954aabce14ee6692b1e6
Lee Kyung Hoon <hjilil@naver.com>
2018-07-23 18:12:55 +0900

    aaaa

---
+++ blockchain/Transaction.cpp
@@ -0,0 +1,22 @@
+#include"Transaction.h"
+#include"cryptopp700/sha.h"
+
+using namespace CryptoPP;
+
+void Transaction::sign_Transaction() {
+	this->sign_Transaction = true;
+}
+
+void Transaction::getHash(const Transaction &Tx ) {
+	std::string txString = "";
+	txString.append(std::to_string(Tx.state_sign));
+	
+
+	this->hash = SHA256::InitState();
+
+}
+
+Transaction Transaction::getTransaction() {
+
+	return TransactionData();
+}
